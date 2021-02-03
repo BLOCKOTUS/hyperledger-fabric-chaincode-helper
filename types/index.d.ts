787e11754fb60d3afcabd67815624316d00ee686
Daniel Febrero <febrero@me.com>
2021-02-03 19:04:38 +0100

    BLOCKOTUS/organism#125 remove unused import

--- types/index.d.ts
+++ types/index.d.ts
@@ -6,6 +6,10 @@ declare module 'hyperledger-fabric-chaincode-helper' {
 
         getTimestamp(ctx: Context): Number;
         getUniqueClientId(ctx: Context): string;
+        didGet(ctx: Context, id: string): string;
+        didPost(ctx: Context, data: any): string;
+        didPut(ctx: Context, id: string, data: any): string;
+        didDelete(ctx: Context, id: string): string;
         didRequest(ctx: Context, subject: string, method: string, data: string): Promise<string>;
     }
 
