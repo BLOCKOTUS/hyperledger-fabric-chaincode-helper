ed68691c685ef6da69a6773046dba9942275a193
Daniel Febrero <febrero@me.com>
2021-02-16 10:59:45 +0100

    add exists() function

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,6 +8,7 @@ declare module 'hyperledger-fabric-chaincode-helper' {
         didPut(ctx: Context, id: string, data: any): Promise<string>;
         didDelete(ctx: Context, id: string): Promise<string>;
         didRequest(ctx: Context, subject: string, method: string, data: string): Promise<string>;
+        exists(ctx: Context, id: string): Promise<boolean>;
         getTimestamp(ctx: Context): Number;
         getUniqueClientId(ctx: Context): string;
         getParams(ctx: Context, rules?: { length?: number }): Array<string>;
