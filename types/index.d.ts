4f19429dfbe5d1cabba5bc1444e5cc25a976c486
Daniel Febrero <febrero@me.com>
2021-02-16 11:10:57 +0100

    add getAllResultsFromIterator() function

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,15 @@
 declare module 'hyperledger-fabric-chaincode-helper' {
 
     import type { Context, Contract } from 'fabric-contract-api';
+    import type { Iterators } from 'fabric-shim-api';
+
+    type QueryResult = {
+        Key: string | null,
+        TxId: string | null,
+        Timestamp: number | null,
+        Value: string | object | null,
+        Record: string | object | null,
+    };
 
     export class BlockotusContract extends Contract {
         didGet(ctx: Context, id: string): Promise<string>;
@@ -9,6 +18,7 @@ declare module 'hyperledger-fabric-chaincode-helper' {
         didDelete(ctx: Context, id: string): Promise<string>;
         didRequest(ctx: Context, subject: string, method: string, data: string): Promise<string>;
         exists(ctx: Context, id: string): Promise<boolean>;
+        getAllResultsFromIterator(iterator: Iterators.StateQueryIterator, isHistory?: boolean, limit?: number): Promise<Array<QueryResult>>;
         getTimestamp(ctx: Context): Number;
         getUniqueClientId(ctx: Context): string;
         getParams(ctx: Context, rules?: { length?: number }): Array<string>;
